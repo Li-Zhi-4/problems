bc49d031896d5dc7a3df3b8234a9142ca8bb286a
Li Zhi <60483180+Li-Zhi-4@users.noreply.github.com>
2026-09-10 21:27:00 -0400

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        track = set()
+
+        for n in nums:
+            if n in track:
+                return True
+            track.add(n)
+        
+        return False
---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = "".join(c for c in s if c.isalnum()).lower()
+        left, right = 0, len(cleaned) - 1
+
+        while (left <= right):
+            print(f"{cleaned[left]} == {cleaned[right]}")
+            if cleaned[left] == cleaned[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        
+        return True
